6d2aa8c3f0b2d3a8b4acd0fd14bd834d8a463555
hxx0215 <hxx0215@126.com>
2016-02-02 21:32:56 +0800

    just change to proxy

---
+++ SimpleTunnelCustomizedNetworkingUsingtheNetworkExtensionFramework 2/SimpleTunnel/ProxyServer.swift
@@ -0,0 +1,13 @@
+//
+//  ProxyServer.swift
+//  SimpleTunnel
+//
+//  Created by hxx on 16/2/2.
+//  Copyright © 2016年 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class ProxyServer: NSObject {
+
+}
